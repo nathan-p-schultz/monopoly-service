53a540ade30eeb6fcb7c95d4b86188574a57626c
kvlinden <kvlinden@calvin.edu>
2024-11-04 08:56:58 -0500

    ported monopolyDirect to Azure PostgreSQL

--- monopolyDirect.js
+++ monopolyDirect.js
@@ -1,17 +1,19 @@
 /* eslint-disable no-console */
 /**
  * This module implements direct, Android-to-Postgres access to the Monopoly DB.
- * The database is hosted on ElephantSQL.
+ * The database is hosted on Azure PostgreSQL. I needed to add my local public
+ * IP address to the Azure networking firewall access list.
  *
  * Because the PGP connection variables are stored as Heroku config vars, store
  * those values in .env (stored locally and listed in .gitignore so that they're
- * not pushed to GitHub, e.g., one line would be DB_PORT=5432) and the run this
- * module using the Procfile script:
+ * not pushed to GitHub, e.g., one line would be DB_PORT=5432).
  *
- *      heroku local direct
+ *      source .env-azure
+ *      node monopolyDirect.js
  *
  * @author: kvlinden
  * @date: Summer, 2020
+ * @date: Fall, 2024 - updates for Azure PostgreSQL
  */
 
 // Set up the database connection.
@@ -20,7 +22,7 @@ const pgp = require('pg-promise')();
 const db = pgp({
   host: process.env.DB_SERVER,
   port: process.env.DB_PORT,
-  database: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   // For SSL, see: https://stackoverflow.com/questions/22301722/ssl-for-postgresql-connection-nodejs
