153ef0013187353f0b145cc2958e52280c11d179
Nathan Schultz <nps9@calvin.edu>
2024-11-18 17:11:54 -0500

    update

--- monopolyService.js
+++ monopolyService.js
@@ -35,6 +35,7 @@ const db = pgp({
   database: process.env.DB_DATABASE,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
+  ssl: { rejectUnauthorized: false },
 });
 
 // Configure the server and its routes.
